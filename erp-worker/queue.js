7c71e7b546969400da82c472573fe08847034405
Yasser <your@email.com>
2026-07-06 20:47:32 +0300

    chore(control-plane): remove nested service copies

--- erp-worker/queue.js
+++
@@ -1,6 +0,0 @@
-const { Queue } = require("bullmq");
-const connection = require("./redis");
-
-const provisioningQueue = new Queue("erp-provisioning", { connection });
-
-module.exports = provisioningQueue;
